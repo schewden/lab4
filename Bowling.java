df1bebc4c066816d824c4192ca298f0f22949129
Denis <schewden@mail.ru>
2016-12-11 17:40:03 +0300

    Test2

--- Bowling.java
+++ Bowling.java
@@ -2,11 +2,15 @@
  * Created by Денис on 11.12.2016.
  */
 public class Bowling {
+    int score=0;
+
     public void roll(int pins){
+        score += pins;
+
 
     }
     public int score(){
-        return 0;
+        return score;
     }
 
 
--- BowlingTest.java
+++ BowlingTest.java
@@ -6,12 +6,19 @@ import static junit.framework.TestCase.assertEquals;
  * Created by Денис on 11.12.2016.
  */
 public class BowlingTest {
+    Bowling bowling = new Bowling();
     @Test
         public void testCheckProject(){
-        Bowling bowling = new Bowling();
         for (int i=0; i<20;i++){
             bowling.roll(0);
             assertEquals(0, bowling.score());
         }
+    }
+
+    @Test
+        public void testRollOnePin(){
+        for (int i = 0; i < 20; i++)
+            bowling.roll(1);
+        assertEquals(20, bowling.score());
     }
 }
