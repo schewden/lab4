889aceec28e34911caaab3e95a0e76148c5fd013
Denis <schewden@mail.ru>
2016-12-11 21:54:59 +0300

    New test5
    Добавлен новый тест, если выбивают каждый раз страйки(testDropStrikeForTheEntireGame). А так же изменения под тест в score().

---
+++ src/ru/shevelev/bowling/Bowling.java
@@ -0,0 +1,34 @@
+package ru.shevelev.bowling;
+
+/**
+ * Created by Денис on 11.12.2016.
+ */
+public class Bowling {
+    private int rolls[] = new int[21];
+    private int thisRoll = 0;
+
+
+    public void roll(int pins) {
+        rolls[thisRoll++] = pins;
+    }
+
+
+    public int score() {
+        int score = 0;
+        int i = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (rolls[i] == 10) {
+                score += rolls[i] + rolls[i + 1] + rolls[i + 2];
+                i++;
+            } else if (rolls[i] + rolls[i + 1] == 10) {
+                score += rolls[i] + rolls[i + 1] + rolls[i + 2];
+                i += 2;
+            } else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
+        }
+
+        return score;
+    }
+}
