571d9b4b9011948b1a3ac9223ac59fc4b291b529
Denis <schewden@mail.ru>
2016-12-11 20:28:08 +0300

    Test3
    Добавлен тест, который считает число очков, если за два броска сбивается 10 кеглей(testDropTenPin). A так же были исправлены два метода roll()  и score().

--- Bowling.java
+++ Bowling.java
@@ -2,16 +2,29 @@
  * Created by Денис on 11.12.2016.
  */
 public class Bowling {
-    int score=0;
+    private int rolls[] = new int[21];
+    private int thisRoll = 0;
+
 
     public void roll(int pins){
-        score += pins;
-
-
+        rolls[thisRoll++] = pins;
     }
+
+
     public int score(){
-        return score;
+        int score = 0;
+        int i = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (rolls[i] + rolls[i + 1] == 10)
+            {
+                score += 10 + rolls[i + 2];
+                i += 2;
+            } else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
+        }
+
+            return score;
     }
-
-
 }
--- BowlingTest.java
+++ BowlingTest.java
@@ -21,4 +21,15 @@ public class BowlingTest {
             bowling.roll(1);
         assertEquals(20, bowling.score());
     }
+
+    @Test
+        public void testDropTenPin(){
+            bowling.roll(8);
+            bowling.roll(2);
+            bowling.roll(4);
+        for (int i=0; i<17;i++){
+            bowling.roll(0);
+        }
+            assertEquals(18,bowling.score());
+    }
 }
